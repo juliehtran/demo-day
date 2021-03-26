cc4eb404a28dfaa5b3a6bbd07205513cc13a0090
Julie Tran <tranjhdesign@gmail.com>
2021-03-25 22:58:12 -0400

    add database url env var

--- config/database.js
+++ config/database.js
@@ -1,6 +1,5 @@
 // config/database.js
 module.exports = {
-
-    'url' : 'mongodb+srv://demo:demo@cluster0-92dvh.mongodb.net/test?retryWrites=true', 
+    'url' : process.env.DATABASE_URL || 'mongodb+srv://demo:demo@cluster0-92dvh.mongodb.net/test?retryWrites=true', 
     'dbName': 'demo'
 };
